d756a8e59595d874d3051103b016d077ec8a902f
Francesco Amato <fra.amato95@gmail.com>
2021-09-29 17:22:13 +0200

    Refactored training loop and added test

--- data.py
+++ data.py
@@ -14,7 +14,7 @@ class FcgrBags(Dataset):
         path = str(pathlib.Path(__file__).resolve()).removesuffix("\data.py").replace('\\','/') + "/datasets/Drosophila_Promoter.fas"
         sequence_list = sq.sequencer(path)
         bags = []
-        bags_lables = []
+        bags_labels = []
         for i in range(len(sequence_list)):
             #generiamo la rappresentazione in Frequency Chaos Game Representation(fcgr) e creiamo quattro bag per ogni immagine
             matrix = fcgr.getMatrice(sequence_list[i][0].lower(),k=kmers_len)
@@ -28,19 +28,19 @@ class FcgrBags(Dataset):
             bags.append(m4)
             #Creiamo un etichetta per ognuna delle bag create
             if(sequence_list[i][1] == 'nuc'):
-                bags_lables.extend(0 for _ in range(4))
+                bags_labels.extend(0 for _ in range(4))
             else:
-                bags_lables.extend(1 for _ in range(4))
+                bags_labels.extend(1 for _ in range(4))
 
         device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
         self.fcgr_bags = torch.tensor(bags,device=device,dtype=torch.float32,requires_grad=True)
-        self.lables = torch.tensor(bags_lables,device=device,dtype=torch.long)
+        self.labels = torch.tensor(bags_labels,device=device,dtype=torch.long)
         
         self.transform = transform
         self.sample_number = self.fcgr_bags.size()[0]
 
     def __getitem__(self,index):
-        sample = self.fcgr_bags[index],self.lables[index]
+        sample = self.fcgr_bags[index],self.labels[index]
 
         if self.transform != None:
             sample = (self.transform(sample[0],sample[1]))
--- main.py
+++ main.py
@@ -5,49 +5,70 @@ from model import *
 import data
 import math
 
-#parameters
-kmers_len = 3
-learning_rate = 0.001
 
-#Create network model 
-print('creating network...')
-network = MilModel(kmers_len)
+def training(model,train_loader,loss_function,optimizer,number_epochs=50):
+    for epoch in range(number_epochs):
+        print('-------- Start Epoch {} --------'.format(epoch + 1))
+        for i,(bags,labels) in enumerate(train_loader):
+            #Zero gradient parameters
+            optimizer.zero_grad()
 
-#Create datasets
-print('creating dataset...')
-dataset = data.FcgrBags(kmers_len)
-train_size = math.ceil((70 * dataset.sample_number)/100)
-validation_size = math.floor((10 * dataset.sample_number)/100)
-test_size = math.floor((20 * dataset.sample_number)/100)
-train,validation,test = random_split(dataset, [train_size, validation_size,test_size])
-train_loader = DataLoader(train,batch_size=100)
-validation_loader = DataLoader(validation)
-test_loader = DataLoader(test)
+            #forward
+            class_pred = model.forward(bags.unsqueeze(1))
+            #loss
+            loss = loss_function(class_pred,labels)
+            #Backward
+            loss.backward()
+            #update weights and bias
+            optimizer.step()
+            #if (i+1)%100 == 0:
+            #   print( 'Batch {}/1000'.format(i+1))
+        print('Epoch: {} - loss:{}'.format(epoch + 1, loss.item()))
+    return model
 
-#Loss
-print('creating loss...')
-binary_cross_entropy = nn.CrossEntropyLoss()
+def testing(model,test_loader):
+    model.eval() # switch off dropout/batch normalization etc..
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for (bags,labels) in test_loader:
+            output = model(bags.unsqueeze(1))
+            _, predicted = torch.max(output.data, 1)
 
-#optimizer
-print('creating optimizer...')
-optimizer = optim.SGD(network.parameters(), lr=learning_rate,momentum=0.9)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accurecy:', correct / total)
 
-#training loop
-print('training loop...')
-for epoch in range(50):
-    print('-------- Start Epoch {} --------'.format(epoch + 1))
-    for i,(bags,labels) in enumerate(train_loader):
-        #Zero gradient parameters
-        optimizer.zero_grad()
+def main():
+    #parameters
+    kmers_len = 3
+    learning_rate = 0.001
 
-        #forward
-        class_pred = network.forward(bags.unsqueeze(1))
-        #loss
-        loss = binary_cross_entropy(class_pred,labels)
-        #Backward
-        loss.backward()
-        #update weights and bias
-        optimizer.step()
-        #if (i+1)%100 == 0:
-        #   print( 'Batch {}/1000'.format(i+1))
-    print('Epoch: {} - loss:{}'.format(epoch + 1, loss.item()))
+    #Create network model 
+    print('creating network...')
+    network = MilModel(kmers_len)
+
+    #Create datasets
+    print('creating dataset...')
+    dataset = data.FcgrBags(kmers_len)
+    train_size = math.ceil((70 * dataset.sample_number)/100)
+    validation_size = math.floor((10 * dataset.sample_number)/100)
+    test_size = math.floor((20 * dataset.sample_number)/100)
+    train,validation,test = random_split(dataset, [train_size, validation_size,test_size])
+    train_loader = DataLoader(train,batch_size=100)
+    validation_loader = DataLoader(validation)
+    test_loader = DataLoader(test)
+
+    #Loss
+    print('creating loss...')
+    cross_entropy = nn.CrossEntropyLoss()
+
+    #optimizer
+    print('creating optimizer...')
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate,momentum=0.9)
+
+    #training loop
+    model = training(network,train_loader,cross_entropy,optimizer,number_epochs=2)
+    testing(model,test_loader)
+if __name__ == '__main__':
+    main()
